4ec95e4751b1b9ca32aae6b3775ab194ab3aca2c
Gerardo Ochoa <gera.8a.lpz@gmail.com>
2019-02-28 10:20:16 -0600

    Practica2 LDOO
    Aplicar servlet a un formulario.

---
+++ Practica2_LDOO/src/java/Practica2/servlet/Servlet_P2.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author cuack
+ */
+@WebServlet(name = "Servlet_P2", urlPatterns = {"/Servlet_P2"})
+public class Servlet_P2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+ 
+
+    // <editor-fold defaultstate="collapsed" desdc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter sal=response.getWriter();
+            sal.println("<!DOCTYPE html>");
+            sal.println("<html>");
+            sal.println("<head>");
+            sal.println("<title>Datos Introducidos</title>");            
+            sal.println("</head>");
+            sal.println("<body>");
+            sal.println("Email: " + request.getParameter("email") + "<br/>");
+            sal.println("Contraseña: " + request.getParameter("password") + "<br/>");
+            sal.println("Nombre: " + request.getParameter("nombre") + "<br/>");
+            sal.println("Fecha de Nacimiento: " + request.getParameter("fecha") + "<br/>");
+            sal.println("</body>");
+            sal.println("</html>");
+
+    }
+
+   
+
+
+}
---
+++ Practica2_LDOO/web/WEB-INF/Servlet_P2.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica2.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author cuack
+ */
+@WebServlet(name = "Servlet_P2", urlPatterns = {"/Servlet_P2"})
+public class Servlet_P2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+ 
+
+    // <editor-fold defaultstate="collapsed" desdc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter sal=response.getWriter();
+            sal.println("<!DOCTYPE html>");
+            sal.println("<html>");
+            sal.println("<head>");
+            sal.println("<title>Datos Introducidos</title>");            
+            sal.println("</head>");
+            sal.println("<body>");
+            sal.println("Email: " + request.getParameter("email") + "<br/>");
+            sal.println("Contraseña: " + request.getParameter("password") + "<br/>");
+            sal.println("Nombre: " + request.getParameter("nombre") + "<br/>");
+            sal.println("Fecha de Nacimiento: " + request.getParameter("fecha") + "<br/>");
+            sal.println("</body>");
+            sal.println("</html>");
+
+    }
+
+   
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
